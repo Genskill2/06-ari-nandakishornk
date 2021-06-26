86aad4e22be5826340c7b44be513a4e2e6623d8c
Nandakishor <nandakishornk01@gmail.com>
2021-06-26 21:56:31 +0530

    Final commit

--- ari.c
+++ ari.c
@@ -21,31 +21,31 @@ string ari(string s)
 	}
 	float r1=4.71*(c/w)+0.5*(w/st)-21.43;
 	int r=(int)r1;
-	if(r<=1)
+	if(r==1)
 		return "Kindergarten";
-	else if(r<=2)
+	else if(r==2)
 		return "First/Second Grade";
-	else if(r<=3)
+	else if(r==3)
 		return "Third Grade";
-	else if(r<=4)
+	else if(r==4)
 		return "Fourth Grade";
-	else if(r<=5)
+	else if(r==5)
 		return "Fifth Grade";
-	else if(r<=6)
+	else if(r==6)
 		return "Sixth Grade";
-	else if(r<=7)
+	else if(r==7)
 		return "Seventh Grade";
-	else if(r<=8)
+	else if(r==8)
 		return "Eighth Grade";
-	else if(r<=9)
+	else if(r==9)
 		return "Ninth Grade";
-	else if(r<=10)
+	else if(r==10)
 		return "Tenth Grade";
-	else if(r<=11)
+	else if(r==11)
 		return "Eleventh Grade";
-	else if(r<=12)
+	else if(r==12)
 		return "Twelfth grade";
-	else if(r<=13)
+	else if(r==13)
 		return "College student";
 	else
 		return "Professor";
