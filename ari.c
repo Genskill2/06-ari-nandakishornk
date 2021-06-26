a000c2491d1aec0aa8432d8ce9182dde0f2536ba
Nandakishor <nandakishornk01@gmail.com>
2021-06-26 15:19:14 +0530

    First attempt

---
+++ ari.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<math.h>
+
+
+string ari(string s)
+{
+	int i,c=0,w=0,st=0;
+	for(i=0;s[i]!='\0';i++)
+	{
+		if(isalnum(s[i])==1)
+			c++;
+		if(s[i]==' ')
+			w++;
+		if(s[i]=='.' || s[i]=='?' || s[i]=='!')
+			st++;
+	}
+	float r=4.71*(c/w)+0.5*(w/st)-21.43;
+	if(r-roundf(r)>=0.5)	
+		r=roundf(r);
+	else
+		r=roundf(r)+1;
+	if(r<=5.0)
+		return "Error";
+	else if(r<=7.0)
+		return "First/Second Grade";
+	else if(r<=9.0)
+		return "Third Grade";
+	else if(r<=10.0)
+		return "Fourth Grade";
+	else if(r<=11.0)
+		return "Fifth Grade";
+	else if(r<=12.0)
+		return "Sixth Grade";
+	else if(r<=13.0)
+		return "Seventh Grade";
+	else if(r<=14.0)
+		return "Eighth Grade";
+	else if(r<=15.0)
+		return "Ninth Grade";
+	else if(r<=16.0)
+		return "Tenth Grade";
+	else if(r<=17.0)
+		return "Eleventh Grade";
+	else if(r<=18.0)
+		return "Twelfth grade";
+	else if(r<=24.0)
+		return "College student";
+	else
+		return "Professor";
+}
